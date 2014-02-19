514ba6702bb8bc59500c028ae0a4e1d7d5e17953
Ben Reinhart <breinhart@groupon.com>
2014-02-18 22:37:42 -0600

    Reorganize spec structure

--- test/app/assets/search_parser_test.js
+++ test/app/assets/search_parser_test.js
@@ -4,22 +4,26 @@ var expect = require('chai').expect
   , parser = requireRoot('app/assets/js/search_parser');
 
 describe('SearchParser', function(){
-  context('commands', function(){
-    it('correctly parses "shows"', function(){
+  describe('"show" command', function(){
+    it('is correctly parsed', function(){
       var actual = parser.parse('shows')
         , expected = {command: 'shows'};
 
       expect(actual).to.deep.equal(expected);
     });
+  });
 
-    it('correctly parses "song"', function(){
+  describe('"song" command', function(){
+    it('is correctly parsed', function(){
       var actual = parser.parse('song')
         , expected = {command: 'song'};
 
       expect(actual).to.deep.equal(expected);
     });
+  });
 
-    it('correctly parses "venue"', function(){
+  describe('"venue" command', function(){
+    it('is correctly parsed', function(){
       var actual = parser.parse('venue')
         , expected = {command: 'venue'};
 
