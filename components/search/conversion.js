34939c5593bf5a9ef8dec2395539ef04f8f6706d
Ben Reinhart <breinhart@groupon.com>
2014-03-02 17:22:18 -0600

    First pass at converting show search to es query

--- components/search/conversion.js
+++ components/search/conversion.js
@@ -1,3 +1,22 @@
+var $commands, $filters, $qualifiers
+  , _ = require('underscore')
+  , extend = _.extend
+  , canonicalDate = requireRoot('core/lib/utils/date').canonicalDate;
+
+$commands = {};
+$commands['shows'] = $commands['show'] = buildShowQuery;
+
+$filters = {
+  in: buildInFilter,
+  sort: buildSortFilter
+}
+
+$qualifiers = {
+  first: buildFirstQualifier,
+  last: buildLastQualifier
+}
+
+
 /**
  * Converts a parsed search object
  * into an elasticsearch query.
@@ -8,5 +27,164 @@
  */
 
 exports.toQuery = function(search){
+  return $commands[search.command](search);
+}
 
+
+/**
+ * Creates a default skeleton query to
+ * be merged over.
+ *
+ * @return {Object}
+ * @api private
+ */
+
+function buildDefaultQuery(){
+  return {
+    filter: buildDateRange(),
+    size: 20,
+    sort: buildSort('date')
+  }
+}
+
+
+/**
+ * Converts a show search
+ * into an elasticsearch query.
+ *
+ * @param {Object} search
+ * @return {Object}
+ * @api private
+ */
+
+function buildShowQuery(search){
+  var filters = search.filters || []
+    , qualifier = search.qualifier
+    , query;
+
+  query = filters.map(function(filter){
+    return $filters[filter.filter](filter);
+  });
+
+  if ($qualifiers[qualifier])
+    query.push($qualifiers[qualifier]());
+
+  return query.reduce(function(query, clause){
+    return extend(query, clause);
+  }, buildDefaultQuery());
+}
+
+
+/**
+ * Sets `size` to 1
+ *
+ * @return {Object}
+ * @api private
+ */
+
+function buildFirstQualifier(){
+  return {size: 1}
+}
+
+
+/**
+ * Sets `size` to 1,
+ * sort `order` to 'desc'
+ *
+ * @return {Object}
+ * @api private
+ */
+
+function buildLastQualifier(){
+  return {
+    size: 1,
+    sort: buildSort('date', 'desc')
+  }
+}
+
+
+/**
+ * Builds an object for the `in` filter
+ *
+ * @param {Object} filter
+ * @return {Object}
+ * @api private
+ */
+
+function buildInFilter(filter){
+  var year = filter.arguments[0]
+    , from = canonicalDate(new Date(year))
+    , to = canonicalDate(new Date(+year + 1))
+  return {filter: buildDateRange(from, to)};
+}
+
+
+/**
+ * Builds an object for the `sort` filter
+ *
+ * @param {Object} filter
+ * @return {Object}
+ * @api private
+ */
+
+function buildSortFilter(filter){
+  return buildSort('date', filter.sort);
+}
+
+
+/**
+ * Builds a date range. If either
+ * `from` or `to` are left out, it
+ * defaults them to the earliest possible
+ * date (1983-01-01) and the current date,
+ * respectfully.
+ *
+ *
+ * @param {String} from
+ * @param {String} to
+ * @return {Object}
+ * @api private
+ */
+
+function buildDateRange(from, to) {
+  return buildRange('date', {
+    from: (from || '1983-01-01'),
+    to: to || canonicalDate()
+  });
+}
+
+
+/**
+ * Builds a range object.
+ *
+ * @param {String} field
+ * @param {Object} attrs
+ * @return {Object}
+ * @api private
+ */
+
+function buildRange(field, attrs){
+  var range = {range: {}};
+  range.range[field] = attrs;
+  return range;
+}
+
+
+/**
+ * Builds a sort object. Order is
+ * 'asc' by default.
+ *
+ *     buildSort('date', 'desc')
+ *     // => {date: {order: 'desc'}}
+ *
+ * @param {String} field
+ * @param {String} order
+ * @return {Object}
+ * @api private
+ */
+
+function buildSort(field, order) {
+  var sort = {};
+  sort[field] = {order: order || 'asc'}
+  return [sort];
 }
--- test/components/search/conversion_test.js
+++ test/components/search/conversion_test.js
@@ -1,8 +1,83 @@
 require(require('path').join('..', '..', '..', 'core', 'globals'));
 
 var expect = require('chai').expect
-  , toQuery = requireRoot('components/search/conversion').toQuery;
+  , DateStubs = requireRoot('test/support/date_stubs')
+  , stubDate = DateStubs.stubDate
+  , unstubDate = DateStubs.unstubDate
+  , toQuery = requireRoot('components/search/conversion').toQuery
+  , canonicalDate = requireRoot('core/lib/utils/date').canonicalDate;
 
-describe('#toQuery', function(){
+describe('Conversion', function(){
+  describe('#toQuery', function(){
+    describe('#show[s]', function(){
+      beforeEach(function(){
+        stubDate('2014-03-02');
+      });
 
+      afterEach(unstubDate);
+
+      it('will look up all shows', function(){
+        var search = {command: 'shows'}
+          , actual = toQuery(search)
+          , expected = {
+              size: 20,
+              filter: {
+                range: {
+                  date: {
+                    from: '1983-01-01',
+                    to: '2014-03-02'
+                  }
+                }
+              },
+              sort: [
+                {date: {order: 'asc'}}
+              ]
+            };
+
+        expect(actual).to.deep.equal(expected);
+      });
+
+      it('will look up the first show', function(){
+        var search = {command: 'shows', qualifier: 'first'}
+          , actual = toQuery(search)
+          , expected = {
+              size: 1,
+              filter: {
+                range: {
+                  date: {
+                    from: '1983-01-01',
+                    to: '2014-03-02'
+                  }
+                }
+              },
+              sort: [
+                {date: {order: 'asc'}}
+              ]
+            };
+
+        expect(actual).to.deep.equal(expected);
+      });
+
+      it('will look up the last show', function(){
+        var search = {command: 'shows', qualifier: 'last'}
+          , actual = toQuery(search)
+          , expected = {
+              size: 1,
+              filter: {
+                range: {
+                  date: {
+                    from: '1983-01-01',
+                    to: '2014-03-02'
+                  }
+                }
+              },
+              sort: [
+                {date: {order: 'desc'}}
+              ]
+            };
+
+        expect(actual).to.deep.equal(expected);
+      });
+    });
+  });
 });
