1a2e405608d159fe26adf5ca640eb4e71ea4476b
Ben Reinhart <benjreinhart@gmail.com>
2014-05-24 23:48:30 -0500

    Freeze config object

--- core/config.js
+++ core/config.js
@@ -1,8 +1,6 @@
-var config = requireRoot('core/config/index')[process.env.NODE_ENV || 'development']
-  , elasticsearchConfig = requireRoot('core/config/elasticsearch')[process.env.NODE_ENV || 'development']
-  , _ = require('underscore');
+var env = process.env.NODE_ENV || 'development'
+  , config = requireRoot('core/config/index')[env];
 
-module.exports = _.extend(
-  _.clone(config),
-  {elasticsearch: _.clone(elasticsearchConfig)}
-);
+config.elasticsearch = requireRoot('core/config/elasticsearch')[env];
+
+module.exports = Object.freeze(config);
